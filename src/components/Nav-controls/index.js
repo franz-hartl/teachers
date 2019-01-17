05cbd7acc3c50aefae10c6f3f19f2499493aa580
cl898 <kenlu.lu@gmail.com>
2019-01-17 12:25:46 -0500

    redo NavControls index.js

--- src/components/Nav-controls/index.js
+++ src/components/Nav-controls/index.js
@@ -1,64 +1,40 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Link } from 'gatsby'
 
 class NavControls extends React.Component {
   render() {
     return (
-      <Router>
-        <div className="content-nav-button">
-          <Link
-            to="/18.01.01.x"
-            className="btn prv-btn btn-primary mr-2 px-4 btn-sm"
-            role="button"
-            aria-disabled="true"
-          >
-            <i className="fa fa-arrow-left" /> Previous
-          </Link>
-          <Link
-            to="/18.01.01.x"
-            className="btn px-4 btn-primary next-btn btn-sm ml-2"
-            role="button"
-            aria-pressed="true"
-          >
-            Next <i className="fa fa-arrow-right" />
-          </Link>
-          <a
-            href="#"
-            id="backtotop"
-            className="btn px-4 btn-primary backtotop btn-sm ml-2"
-            role="button"
-            aria-pressed="true"
-          >
-            {' '}
-            Back to Top <i className="fa fa-arrow-up" />
-          </a>
-          <Route path="/18.01.01.x.app" component={Prev} />
-          <Route path="/18.01.02.x" component={Next} />
-        </div>
-      </Router>
+      <div className="content-nav-button">
+        <Link
+          to="/curriculum/guides/2018/1/18.01.01.x.html"
+          class="btn prv-btn btn-primary mr-2 px-4 btn-sm"
+          role="button"
+          aria-disabled="true"
+        >
+          <i className="fas fa fa-arrow-left" /> Previous
+        </Link>
+        <Link
+          to="#"
+          class="btn px-4 btn-primary next-btn btn-sm ml-2"
+          role="button"
+          aria-pressed="true"
+        >
+          Next
+          <i className="fas fa fa-arrow-right" />
+        </Link>
+        <a
+          href="#"
+          id="backtotop"
+          class="btn px-4 btn-primary backtotop btn-sm ml-2"
+          role="button"
+          aria-pressed="true"
+        >
+          Back to Top
+          <i className="fas fa fa-arrow-up" />
+        </a>
+      </div>
     )
   }
-}
-function Prev() {
-  return (
-    <div>
-      <Link to={'${match.url}/testprevurl'}> Test Prev Button</Link>
-
-      <Route path={`${match.path}/:topicId`} component={Prev} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  )
-}
-function Next() {
-  return (
-    <div>
-      <Link to={'${match.url}/testnexturl'}> Test Next Button</Link>
-    </div>
-  )
 }
 
 export default NavControls
