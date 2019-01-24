265ea7033e082aceb285183b68139de393750c28
cl898 <kenlu.lu@gmail.com>
2019-01-24 15:15:22 -0500

    add bugherd

--- src/html.js
+++ src/html.js
@@ -23,6 +23,16 @@ export default class HTML extends React.Component {
           />
 
           <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function (d, t) {
+  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  bh.type = 'text/javascript';
+  bh.src = 'https://www.bugherd.com/sidebarv2.js?apikey=j6cceeknwiplgovqcodexg';
+  s.parentNode.insertBefore(bh, s);
+  })(document, 'script');`,
+            }}
+          />
         </head>
         <body>
           <div
