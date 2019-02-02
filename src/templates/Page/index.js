397a6ca6af979d20f857224e89d241c871587982
Scrimenti <alan.scrimenti@yale.edu>
2019-02-02 17:03:04 -0500

    added react-icons and 2018 images

--- src/templates/Page/index.js
+++ src/templates/Page/index.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import Layout from 'components/Layout'
+import { IconContext } from 'react-icons'
+import {
+  FaArrowRight,
+  FaArrowLeft,
+  FaDrupal,
+  FaAngleLeft,
+  FaAngleRight,
+  FaDownload,
+  FaFilePdf,
+  FaPrint,
+  FaEnvelope,
+} from 'react-icons/fa'
 
 class PageTemplate extends React.Component {
   render() {
     const { html } = this.props.pageContext.node
     const { title } = this.props.pageContext.node.frontmatter
+
     return (
       <Layout>
-          <div dangerouslySetInnerHTML={{ __html: html }} />
+        <IconContext.Provider value={{ color: '#2B50A3', size: '3em' }}>
+          <div>
+            <FaArrowRight />
+            <FaArrowLeft />
+            <FaAngleLeft />
+            <FaAngleRight />
+            <FaEnvelope />
+            <FaDownload />
+            <FaFilePdf />
+            <FaPrint />
+          </div>
+        </IconContext.Provider>
+
+        <div dangerouslySetInnerHTML={{ __html: html }} />
       </Layout>
     )
   }
