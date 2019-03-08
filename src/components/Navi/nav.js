b13fc87c64441aaf597710a86539027a793d52d0
Scrimenti <alan.scrimenti@yale.edu>
2019-03-08 16:34:31 -0500

    add back all units

--- src/components/Navi/nav.js
+++ src/components/Navi/nav.js
@@ -1,75 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Nav, Navbar, NavDropdown, NavItem } from 'react-bootstrap'
 
 const Navi = () => (
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon" />
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ynhti-nav">
-        <li class="nav-item">
-          <Link class="nav-link" to="#">
-            Curricular Resources <span class="sr-only">(current)</span>
-          </Link>
-        </li>
-        <li class="nav-item dropdown">
-          <Link
-            class="nav-link dropdown-toggle"
-            to="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Brochures and Reports
-          </Link>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <Link class="dropdown-item" to="#">
-              Item 1
-            </Link>
-            <div class="dropdown-divider" />
-            <Link class="dropdown-item" to="#">
-              Item 2
-            </Link>
-            <div class="dropdown-divider" />
-            <Link class="dropdown-item" to="#">
-              Item 3
-            </Link>
-          </div>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="#">
-            Publications
-          </Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="#">
-            Event Proceedings
-          </Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="#">
-            Centers
-          </Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="#">
-            About the Institute
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+ <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav variant="tabs" className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown as={NavItem} href="https://www.google.com" title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
 )
 
 export default Navi
