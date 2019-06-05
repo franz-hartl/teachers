d68a6cc906257e66a17a393f4694f43ab8f870d5
Scrimenti <alan.scrimenti@yale.edu>
2019-06-05 15:30:14 -0400

    removed from html.js: fontawesome, props.headComponents, props.postBodyComponents and bugherd

--- src/html.js
+++ src/html.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import 'font-awesome/css/font-awesome.css'
-import './styles/global.css'
+// import './styles/global.css'
 
 export default class HTML extends React.Component {
   render() {
@@ -14,18 +14,6 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          {this.props.headComponents}
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"></link>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function (d, t) {
-  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
-  bh.type = 'text/javascript';
-  bh.src = 'https://www.bugherd.com/sidebarv2.js?apikey=j6cceeknwiplgovqcodexg';
-  s.parentNode.insertBefore(bh, s);
-  })(document, 'script');`,
-            }}
-          />
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
         </head>
         <body>
@@ -33,7 +21,6 @@ export default class HTML extends React.Component {
             id="___gatsby"
             dangerouslySetInnerHTML={{ __html: this.props.body }}
           />
-          {this.props.postBodyComponents}
         
         </body>
       </html>
