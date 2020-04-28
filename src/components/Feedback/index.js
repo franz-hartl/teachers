d2748a326276106c91bd530648cf3ac92901ae43
Scrimenti <alan.scrimenti@yale.edu>
2020-04-28 08:22:31 -0400

    added/updated text on feedback and search-results pages

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -5,12 +5,10 @@ class Survey_feedback extends React.Component {
     return (
       <div className="p-3 feedback">
         <div className="fbwrap">
-          <p className="fdback">Your feedback is very important to us!</p>
+          <p className="fdback">Your feedback is important to us!</p>
           <p>
-            When you are finished viewing curriculum units on this website,
-            please take a few minutes to provide feedback and help us understand
-            how these units, which were created by public school teachers, are
-            useful to others.
+          After viewing our curriculum units, please take a few minutes to help us understand how 
+          the units, which were created by public school teachers, may be useful to others.
           </p>
         </div>
         <a
--- src/pages/search-results.js
+++ src/pages/search-results.js
@@ -10,6 +10,9 @@ render() {
       <main>
       <div className="main_content m-4">
         <h1 className="page-title mb-3">Search Results</h1>
+        <p>By selecting a tab below you can view the results of your search for the entire site, for curriculum units, for the guide to units, or for both guide entries and units. 
+        You then can sort the results by relevance or by the date units were written.
+        </p>
         <span dangerouslySetInnerHTML={{
         __html: `<script async src="https://cse.google.com/cse.js?cx=014177378021644155909:u_pjrcixyzi"></script><div class="gcse-searchresults-only" data-queryParameterName="q"></div>`
       }}></span>
