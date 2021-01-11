faa6f8869607f62ba2e3178514c1e2f77ef554bf
Devendra Jadhav <devendra.jadhav@yale.edu>
2021-01-11 21:05:02 +0000

    Updates to participate page

--- src/pages/participate/index.js
+++ src/pages/participate/index.js
@@ -7,8 +7,7 @@ const part = () => (
       <div className="container px-5 region-content">
         <h1 className="page-title">Participate</h1>
             <p>The Institute accepts as Fellows teachers of English, history, languages, arts, science, and mathematics from New Haven's public elementary and secondary schools. Interested teachers discuss their potential eligibility with a School Representative or a Steering Committee member who works closely with those teachers who then submit an online application. To learn more about participating in the Institute, see our <a href="/brochures/A1/Brochure-2021.pdf">2021 Annual Brochure</a> and <a href="/brochures/A1/seminar-descriptions-2021.pdf">Seminar Descriptions.</a></p>
-            <p>New Haven Public Schools teachers and administrators are invited to <a target="_blank" href="https://yalesurvey.ca1.qualtrics.com/jfe/form/SV_5BFzuwvQDlKtoLr">register</a> to attend our Virtual Open House at 4:15 p.m. on Tuesday, January 5, 2021, when seminar leaders will describe and answer questions about the seminars they will lead in 2021.</p>
-            <p>
+            <p>New Haven Public Schools teachers and administrators were invited to register to attend our Virtual Open House at 4:15 p.m. on Tuesday, January 5, 2021, when seminar leaders described and answered questions about the seminars they will lead in 2021:<br/>
             <ul>
                 <li><a href="https://youtu.be/--qDJjHY06M" target="_blank">“The Social Struggles of Contemporary Black Art,” led by Roderick Ferguson</a></li>
                 <li><a href="https://youtu.be/7PxntUc9_2Y" target="_blank">“Developing Anti-Racist Curriculum and Pedagogy,” led by Daniel Martinez HoSang</a></li>
