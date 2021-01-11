6d8c0a2a1c3e11ba6d88a8d61ad62cc9a959f7db
Devendra Jadhav <devendra.jadhav@yale.edu>
2021-01-11 17:54:21 +0000

    Updated participate page with video links

--- src/pages/participate/index.js
+++ src/pages/participate/index.js
@@ -8,6 +8,14 @@ const part = () => (
         <h1 className="page-title">Participate</h1>
             <p>The Institute accepts as Fellows teachers of English, history, languages, arts, science, and mathematics from New Haven's public elementary and secondary schools. Interested teachers discuss their potential eligibility with a School Representative or a Steering Committee member who works closely with those teachers who then submit an online application. To learn more about participating in the Institute, see our <a href="/brochures/A1/Brochure-2021.pdf">2021 Annual Brochure</a> and <a href="/brochures/A1/seminar-descriptions-2021.pdf">Seminar Descriptions.</a></p>
             <p>New Haven Public Schools teachers and administrators are invited to <a target="_blank" href="https://yalesurvey.ca1.qualtrics.com/jfe/form/SV_5BFzuwvQDlKtoLr">register</a> to attend our Virtual Open House at 4:15 p.m. on Tuesday, January 5, 2021, when seminar leaders will describe and answer questions about the seminars they will lead in 2021.</p>
+            <p>
+            <ul>
+                <li><a href="https://youtu.be/--qDJjHY06M" target="_blank">“The Social Struggles of Contemporary Black Art,” led by Roderick Ferguson</a></li>
+                <li><a href="https://youtu.be/7PxntUc9_2Y" target="_blank">“Developing Anti-Racist Curriculum and Pedagogy,” led by Daniel Martinez HoSang</a></li>
+                <li><a href="https://youtu.be/0M1ZxsFHAUg" target="_blank">“How to Do Things with Maps,” led by Ayesha Ramachandran</a></li>
+                <li><a href="https://youtu.be/Cq2USfJUt5E" target="_blank">“The Earth’s Greenhouse and Global Warming,” led by Peter Raymond</a></li>
+            </ul>
+            </p>
        </div>
           <div className="row" />
     </main>
