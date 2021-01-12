c8feb3a25a4790c4feb5447c498f7758987f0089
Devendra Jadhav <devendra.jadhav@yale.edu>
2021-01-12 21:40:58 +0000

    Updates to participate page

--- src/pages/participate/index.js
+++ src/pages/participate/index.js
@@ -9,10 +9,34 @@ const part = () => (
             <p>The Institute accepts as Fellows teachers of English, history, languages, arts, science, and mathematics from New Haven's public elementary and secondary schools. Interested teachers discuss their potential eligibility with a School Representative or a Steering Committee member who works closely with those teachers who then submit an online application. To learn more about participating in the Institute, see our <a href="/brochures/A1/Brochure-2021.pdf">2021 Annual Brochure</a> and <a href="/brochures/A1/seminar-descriptions-2021.pdf">Seminar Descriptions.</a></p>
             <p>New Haven Public Schools teachers and administrators were invited to register to attend our Virtual Open House at 4:15 p.m. on Tuesday, January 5, 2021, when seminar leaders described and answered questions about the seminars they will lead in 2021:<br/>
             <ul>
-                <li><a href="https://youtu.be/--qDJjHY06M" target="_blank">“The Social Struggles of Contemporary Black Art,” led by Roderick Ferguson</a></li>
-                <li><a href="https://youtu.be/7PxntUc9_2Y" target="_blank">“Developing Anti-Racist Curriculum and Pedagogy,” led by Daniel Martinez HoSang</a></li>
-                <li><a href="https://youtu.be/0M1ZxsFHAUg" target="_blank">“How to Do Things with Maps,” led by Ayesha Ramachandran</a></li>
-                <li><a href="https://youtu.be/Cq2USfJUt5E" target="_blank">“The Earth’s Greenhouse and Global Warming,” led by Peter Raymond</a></li>
+                <li>
+                    “The Social Struggles of Contemporary Black Art,” led by Roderick Ferguson<br/>
+                    <ul>
+                        <li><a href="/seminars/2021/Ferguson%2C%20Roderick%20Seminar%20Description.pdf" target="_blank">Description</a></li>
+                        <li><a href="https://youtu.be/--qDJjHY06M" target="_blank">Video</a></li>
+                    </ul>
+                </li>
+                <li>
+                    “Developing Anti-Racist Curriculum and Pedagogy,” led by Daniel Martinez HoSang<br/>
+                    <ul>
+                        <li><a href="/seminars/2021/HoSang%2C%20Dan%20Seminar%20Description.pdf" target="_blank">Description</a></li>
+                        <li><a href="https://youtu.be/7PxntUc9_2Y" target="_blank">Video</a></li>
+                    </ul>
+                </li>
+                <li>
+                    “How to Do Things with Maps,” led by Ayesha Ramachandran<br/>
+                    <ul>
+                        <li><a href="/seminars/2021/Ramachandran%2C%20Ayesha%20Seminar%20Description.pdf" target="_blank">Description</a></li>
+                        <li><a href="https://youtu.be/0M1ZxsFHAUg" target="_blank">Video</a></li>
+                    </ul>
+                </li>
+                <li>
+                    “The Earth’s Greenhouse and Global Warming,” led by Peter Raymond<br/>
+                    <ul>
+                        <li><a href="/seminars/2021/Raymond%2C%20Peter%20Seminar%20Description.pdf" target="_blank">Description</a></li>
+                        <li><a href="https://youtu.be/Cq2USfJUt5E" target="_blank">Video</a></li>
+                    </ul>
+                </li>
             </ul>
             </p>
        </div>
