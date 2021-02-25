a92735200de47b547909c02a089af1fae0cabe4b
Devendra Jadhav <devendra.jadhav@yale.edu>
2021-02-25 20:56:47 +0000

    2011 year guide

--- src/pages/curriculum/units/index.js
+++ src/pages/curriculum/units/index.js
@@ -602,6 +602,11 @@ const VolumeIndexPage = () => (
 
                 <td />
               </tr>
+              <tr>
+                <td colSpan="3" style={{padding: "0 20px"}}>
+                  <Link to="/curriculum/guides/2011/">Guide to Curriculum Units</Link>
+                </td>
+              </tr>
 
               <tr valign="top">
                 <td nowrap="nowrap">Volume I</td>
