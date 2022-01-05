38bad0400717f4e90c762183607c9d439d8f7033
ddj9 <jonathan.owens@yale.edu>
2022-01-05 20:27:18 +0000

    Added 2022 Seminar Leader videos - Participate page

--- src/pages/participate/index.js
+++ src/pages/participate/index.js
@@ -6,28 +6,28 @@ const part = () => (
     <main>
       <div className="container px-5 region-content">
         <h1 className="page-title">Participate</h1>
-            <p>The Institute accepts as Fellows teachers of English, history, languages, arts, science, and mathematics from New Haven's public elementary and secondary schools. Interested teachers discuss their potential eligibility with a School Representative or a Steering Committee member who works closely with those teachers who then submit an online application. To learn more about participating in the Institute, see our <a href="/brochures/A1/Brochure-2022.pdf">2022 Annual Brochure</a>. and <a href="/brochures/A1/seminar-descriptions-2022.pdf">Seminar Descriptions.</a></p>
+            <p>The Institute accepts as Fellows teachers of English, history, languages, arts, science, and mathematics from New Haven's public elementary and secondary schools. Interested teachers discuss their potential eligibility with a School Representative or a Steering Committee member who works closely with those teachers who then submit an online application. To learn more about participating in the Institute, see our <a href="/brochures/A1/Brochure-2022.pdf">2022 Annual Brochure</a> and <a href="/brochures/A1/seminar-descriptions-2022.pdf">Seminar Descriptions.</a></p>
             {/*<p>New Haven Public Schools teachers and administrators were invited to register to attend our Virtual Open House at 4:15 p.m. on Tuesday, January 5, 2021, when seminar leaders described and answered questions about the seminars they will lead in 2021:<br/>*/}
             <ul>
                 <li>
                     “Writing About Nature,” led by Jill Campbell, Professor of English<br/>
                     <ul>
                         <li><a href="/seminars/2022/Campbell%20Jill%20Seminar%20Description.pdf" target="_blank">Description</a></li>
-                        {/*<li><a href="https://youtu.be/bPmooMXNpJo" target="_blank">Video</a></li>*/}
+                        <li><a href="https://youtu.be/QIPKsOy6FpM" target="_blank">Video</a></li>
                     </ul>
                 </li>
                 <li>
                     “The Long Fight for a Free Caribbean, 1700s-1959,” led by Anne Eller, Associate Professor of History<br/>
                     <ul>
                         <li><a href="/seminars/2022/Eller%20Ann%20Seminar%20Description.pdf" target="_blank">Description</a></li>
-                        {/*<li><a href="https://youtu.be/p9PIVKbJvCE" target="_blank">Video</a></li>*/}
+                        <li><a href="https://youtu.be/QDzxB3KtENQ" target="_blank">Video</a></li>
                     </ul>
                 </li>
                 <li>
                     “Ethnic Studies: Theory, Practice, and Pedagogy,” led by Daniel Martinez HoSang, Associate Professor of Ethnicity, Race, and Migration, and of American Studies<br/>
                     <ul>
                         <li><a href="/seminars/2022/HoSang%20Dan%20Seminar%20Description.pdf" target="_blank">Description</a></li>
-                        {/*<li><a href="https://youtu.be/BB9YgnTAdh8" target="_blank">Video</a></li>*/}
+                        <li><a href="https://youtu.be/7BMnWpkFdxY" target="_blank">Video</a></li>
                     </ul>
                 </li>
             </ul>
